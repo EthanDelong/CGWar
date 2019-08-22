fbc27c05ba9b7ab197b414b1557a4f3370ee1b0d
EthanDelong <48994953+EthanDelong@users.noreply.github.com>
2019-08-21 22:43:23 -0400

    add card equals test

---
+++ CGWarTests/CardTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CGWar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGWar.Tests
+{
+    [TestClass()]
+    public class CardTests
+    {
+        /// <summary>
+        /// Card comparison testing since we wrote a custom equal function that will be critical to war plays.
+        /// </summary>
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.AreEqual(new Card(Rank.D2, Suit.Clubs), new Card(Rank.D2, Suit.Clubs));
+            Assert.AreEqual(new Card(Rank.Ace, Suit.Spades), new Card(Rank.Ace, Suit.Spades));
+            Assert.AreNotEqual(new Card(Rank.D3, Suit.Diamonds), new Card(Rank.D2, Suit.Clubs));
+            Assert.AreNotEqual(101, new Card(Rank.D2, Suit.Clubs));
+        }
+    }
+}
